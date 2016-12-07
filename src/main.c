2ee6d7ffa318ab07818255f3c84b720c7ac5538e
Artem <Artem>
2016-12-07 12:29:28 +0300

    pressed joy_center

--- src/main.c
+++ src/main.c
@@ -13,26 +13,101 @@
 #include "stm32l476g_discovery.h"
 			
 
-int main(void)
-{
+#define JOY_CENTER_PORT            	GPIOA
+#define JOY_CENTER_PIN             	GPIO_PIN_0
+#define JOY_PORT_CLK		        __HAL_RCC_GPIOA_CLK_ENABLE()
 
-	__HAL_RCC_GPIOB_CLK_ENABLE();
+#define LED_RED_PORT            	GPIOB
+#define LED_RED_PIN             	GPIO_PIN_2
+#define LED_RED_PORT_CLK			__HAL_RCC_GPIOB_CLK_ENABLE()
+
+#define LED_GREEN_PORT            	GPIOE
+#define LED_GREEN_PIN             	GPIO_PIN_8
+#define LED_GREEN_PORT_CLK			__HAL_RCC_GPIOE_CLK_ENABLE()
+
+
+#define ON            				1
+#define OFF            				0
+
+
+uint8_t joy_center_pressed = 0;
+
+
+void gpio_init(void)
+{
+	JOY_PORT_CLK;
+	LED_RED_PORT_CLK;
+	LED_GREEN_PORT_CLK;
 
 	GPIO_InitTypeDef GPIO_InitStruct;
 
+
 	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-	GPIO_InitStruct.Pin = GPIO_PIN_2;
+	GPIO_InitStruct.Pin = LED_RED_PIN;
 	GPIO_InitStruct.Pull = GPIO_NOPULL;
 	GPIO_InitStruct.Speed = GPIO_SPEED_LOW;
+	HAL_GPIO_Init(LED_RED_PORT, &GPIO_InitStruct);
 
-	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+	GPIO_InitStruct.Pin = LED_GREEN_PIN;
+	HAL_GPIO_Init(LED_GREEN_PORT, &GPIO_InitStruct);
 
 
-	for(;;)
+	GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+	GPIO_InitStruct.Pin = JOY_CENTER_PIN;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_LOW;
+	HAL_GPIO_Init(JOY_CENTER_PORT, &GPIO_InitStruct);
+}
+
+
+void led_red(uint8_t on_off)
+{
+	if (!on_off)
 	{
-		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, GPIO_PIN_SET);
-		HAL_Delay(2000);
-		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, GPIO_PIN_RESET);
-		HAL_Delay(2000);
+		HAL_GPIO_WritePin(LED_RED_PORT, LED_RED_PIN, GPIO_PIN_RESET);
+	} else {
+		HAL_GPIO_WritePin(LED_RED_PORT, LED_RED_PIN, GPIO_PIN_SET);
+	}
+}
+
+
+void led_green(uint8_t on_off)
+{
+	if (!on_off)
+	{
+		HAL_GPIO_WritePin(LED_GREEN_PORT, LED_GREEN_PIN, GPIO_PIN_RESET);
+	} else {
+		HAL_GPIO_WritePin(LED_GREEN_PORT, LED_GREEN_PIN, GPIO_PIN_SET);
+	}
+}
+
+
+int main(void)
+{
+	gpio_init();
+
+	while(1)
+	{
+
+		if (HAL_GPIO_ReadPin(JOY_CENTER_PORT, JOY_CENTER_PIN)) {
+
+			joy_center_pressed ^= 1;
+		}
+
+		switch (joy_center_pressed)
+		{
+			case ON:
+			{
+				led_red(ON);
+				break;
+			}
+			case OFF:
+			{
+				led_red(OFF);
+				break;
+			}
+		}
+
 	}
 }
